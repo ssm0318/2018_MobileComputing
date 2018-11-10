ca137f36f8644955b96c8ed9aa799f95bfb179a1
ssm0318 <kleenex0318@gmail.com>
2018-11-11 00:58:46 +0900

    added default image, image upload complete

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,14 @@ class Profile < ApplicationRecord
     mount_uploader              :profile_pic, ProfilePicUploader
     has_and_belongs_to_many     :tags
     belongs_to                  :user
+
+    after_create :add_default_profile_pic
+
+    private
+  
+    def add_default_profile_pic
+      random = rand(1..6)
+      self.profile_pic = Rails.root.join("app/assets/images/profile" + random.to_s + ".png").open
+      self.save!
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,20 +14,6 @@ class User < ApplicationRecord
 
   has_many    :event_requests
 
-  # # event requests
-  # has_many :received_requests, :class_name => "EventRequest", :foreign_key => "requested_id", dependent: :destroy
-  # has_many :requesters, through: :received_requests, dependent: :destroy
-
-  # has_many :made_requests, :class_name => "EventRequest", :foreign_key => "requester_id", dependent: :destroy
-  # has_many :requesteds, through: :made_requests, dependent: :destroy  
-
-  # # event waiting
-  # has_many    :event_waitings, :class_name => "EventWaiting", :foreign_key => "host_id", dependent: :destroy
-  # has_many    :waiters, :through => :event_waitings, dependent: :destroy
-
-  # has_many    :waiting_events, :class_name => "EventWaiting", :foreign_key => "waiter_id", dependent: :destroy
-  # has_many    :hosts, :through => :waiting_events, dependent: :destroy
-
   # follow
   has_many    :followed_follows, :class_name => "Follow", :foreign_key => "followed_id", dependent: :destroy
   has_many    :followers, :through => :followed_follows, dependent: :destroy
